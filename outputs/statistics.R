43bc9ff423bf99dfcf93fcb3b4f3cd5975c67843
oxford-student <144183460+oxford-student@users.noreply.github.com>
2023-09-06 22:26:10 -0700

    Add files via upload

--- outputs/statistics.R
+++ outputs/statistics.R
@@ -5,25 +5,36 @@ results$rel_div_f <- results$div_f/results$rich_f
 
 results_stats <- data.frame(18,1)
 
+# Preliminary plots
+
 barplot(results[which(results$disp==0 & results$evo==0),]$abun_f, names.arg=c("2030/2.6", "2050/2.6", "2030/4.5", "2050/4.5", "2030/8.5", "2050/8.5"))
 barplot(results[which(results$disp==0 & results$evo==0),]$rich_f, names.arg=c("2030/2.6", "2050/2.6", "2030/4.5", "2050/4.5", "2030/8.5", "2050/8.5"))
 
 mean_abun <- aggregate(abun_f ~ habitat+rcp+disp+evo, data=results, FUN=mean)
 sd_abun <- aggregate(abun_f ~ habitat+rcp+disp+evo, data=results, FUN=sd)
 sd_abun$se <- sd_abun$abun_f/sqrt(10)
+sd_abun$ci <- (sd_abun$abun_f * 2.262)/sqrt(10) # calculation of confidence interval
+                                                # based on small sample size correction
+                                                # with 9 degrees of freedom
 
 mean_rich <- aggregate(rich_f ~ habitat+rcp+disp+evo, data=results, FUN=mean)
 sd_rich <- aggregate(rich_f ~ habitat+rcp+disp+evo, data=results, FUN=sd)
 sd_rich$se <- sd_rich$rich_f/sqrt(10)
+sd_rich$ci <- (sd_rich$rich_f * 2.262)/sqrt(10)
+
 
 mean_div <- aggregate(div_f ~ habitat+rcp+disp+evo, data=results, FUN=mean)
 sd_div <- aggregate(div_f ~ habitat+rcp+disp+evo, data=results, FUN=sd)
 sd_div$se <- sd_div$div_f/sqrt(10)
+sd_div$ci <- (sd_div$div_f * 2.262)/sqrt(10)
 
 mean_reldiv <- aggregate(rel_div_f ~ habitat+rcp+disp+evo, data=results, FUN=mean)
 sd_reldiv <- aggregate(rel_div_f ~ habitat+rcp+disp+evo, data=results, FUN=sd)
 sd_reldiv$se <- sd_reldiv$rel_div_f/sqrt(10)
+sd_reldiv$ci <- (sd_reldiv$rel_div_f * 2.262)/sqrt(10)
 
+
+# Output bar plots
 par(mar= c(5,6,4,2), cex.lab=1.5)
 barplot(mean_abun$abun_f, main="Abundance", 
          
@@ -45,11 +56,18 @@ barplot(mean_reldiv$rel_div_f, main="Divergence",
         ylab="Per species years of population isolation", cex.main=2,space=c(2,0.1,0.1,0.1,0.1,0.1),
         col=c("lightblue", "lightblue", "orange", "orange", "darkred", "darkred","lightblue", "lightblue", "orange", "orange", "darkred", "darkred","lightblue", "lightblue", "orange", "orange", "darkred", "darkred"))
 
+
+
 stats_data <- mean_rich
 stats_data$SE_rich <- sd_rich$se
+stats_data$ci_rich <- sd_rich$ci
 stats_data$abun_f <- mean_abun$abun_f
+stats_data$ci_abun <- sd_abun$ci
 stats_data$SE_abun <- sd_abun$se
 stats_data$reldiv <- mean_reldiv$rel_div_f
 stats_data$SE_reldiv <- sd_reldiv$se
-stats_data[c(1:6, 13:18), 9:10] <- NA
+stats_data$ci_reldiv <- sd_reldiv$ci
+stats_data[c(1:6, 13:18), 11:13] <- NA
+
+
 write.csv(stats_data, file="summary_statistics.csv")
